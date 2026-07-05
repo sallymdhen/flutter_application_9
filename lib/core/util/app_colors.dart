57c472ad34b904ab70400b590b5f1c5ee4db2e6b
sallymdhen <ahlammdhen@gmail.com>
2026-07-05 06:13:52 +0300

    Updated project files

---
+++ lib/core/util/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color blackColor = Color(0xFF2B2B2B);
+  static const Color greenColor = Colors.green;
+  static const Color greyColor  = Color(0xFF707B81);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color redColor = Color(0xFFFF1900);
+  
+
+
+}
