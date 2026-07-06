62521df9619135f76536b6059aba943966143f75
sallymdhen <ahlammdhen@gmail.com>
2026-07-06 21:36:33 +0300

    Update app_text_style.dart
    Update app_text_style with new code

---
+++ lib/core/util/app_text_style.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_9/core/util/app_colors.dart';
+import 'package:flutter_application_9/core/util/app_fonts.dart';
+
+class AppTextStyle {
+  static TextStyle hintTRegisterField=
+
+   TextStyle(fontSize: 16,
+            fontFamily: AppFonts.raleway,
+            color: AppColors.blackColor,
+            fontWeight: FontWeight.w500,          
+
+   );
+
+  static TextStyle logRegStyle=
+
+   TextStyle(fontSize: 14,
+            fontFamily: AppFonts.raleway,
+            color: AppColors.whiteColor,
+            fontWeight: FontWeight.w600,
+
+    ); 
+  static TextStyle secondryButtonStyle=
+
+   TextStyle(fontSize: 14,
+            fontFamily: AppFonts.raleway,
+            color: AppColors.blackColor,
+            fontWeight: FontWeight.w600,
+
+    );  
+
+static TextStyle titleProfileStyle=
+
+   TextStyle(fontSize: 20,
+            fontFamily: AppFonts.raleway,
+            color: AppColors.blackColor,
+            fontWeight: FontWeight.w600,
+
+    );        
+
+static TextStyle labelProfileStyle=
+
+   TextStyle(fontSize: 16,
+            fontFamily: AppFonts.raleway,
+            color: AppColors.greyColor,
+            fontWeight: FontWeight.w600,
+
+    );    
+
+static TextStyle paraInDetlsStyle=
+
+   TextStyle(fontSize: 14,
+            fontFamily: AppFonts.mainFont,
+            color: AppColors.greyColor,
+            fontWeight: FontWeight.w400,
+
+    );            
+
+
+
+
+
+
+}
