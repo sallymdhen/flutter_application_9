b343a9ab96d661af074be8c8a9d62e3835ab8dc4
Maya Ali <maya@example.com>
2026-07-09 02:09:02 -0800

    feat: upload favorite screens and widgets to shared core

---
+++ lib/features/widgets/favorite_grid_view.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_9/features/presentation/views/favorite_screen.dart';
+import 'package:flutter_application_9/features/widgets/favorite_product_card.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class FavoriteGridView extends StatelessWidget {
+  final VoidCallback onItemRemoved;
+
+  const FavoriteGridView({
+    super.key,
+    required this.onItemRemoved,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final favoriteProducts = FavoriteManager.favoriteProducts;
+
+    if (favoriteProducts.isEmpty) {
+      return const Center(
+        child: Text(
+          'No favorite products yet!',
+          style: TextStyle(color: Colors.grey),
+        ),
+      );
+    }
+
+    return GridView.builder(
+      itemCount: favoriteProducts.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 21.w,
+        mainAxisSpacing: 20.h,
+        childAspectRatio: 159 / 217,
+      ),
+      itemBuilder: (context, index) {
+        return FavoriteProductCard(
+          product: favoriteProducts[index],
+          onRemoveTap: onItemRemoved,
+        );
+      },
+    );
+  }
+}
